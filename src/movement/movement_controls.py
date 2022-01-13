35e16aaddb231c5e18d9ccdaa6b0ffc1e7479ad1
Rugshan <56658716+Rugshan@users.noreply.github.com>
2022-01-12 22:15:52 -0500

    created movement functions directory

---
+++ src/movement/movement_controls.py
@@ -0,0 +1,2 @@
+# This file can be used to write several functions (defs) containing finite movement/control of the robot's tracks.
+# Could make the functions have default values, and accept arguments if specific values are required (i.e. turnLeft(units: 5))
