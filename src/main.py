11566572df8a699842446df0e01a84fa73e12aba
Rugshan Gnanandram <rugshan@hotmail.com>
2022-02-02 17:30:40 -0500

    added input as an environment variable

--- src/main.py
+++ src/main.py
@@ -3,9 +3,13 @@ import os
 import speech_recognition as sr
 from utilities.error_handler import noalsaerr
 
+# Input Index
+from decouple import config
+input_index = int(config('INPUT_INDEX'))
+
 # Set up Speech Recognizer.
 r = sr.Recognizer()
-speech = sr.Microphone(device_index = 1)
+speech = sr.Microphone(device_index = input_index)
 
 # Clear PyAudio spam.
 os.system('cls' if os.name == 'nt' else 'clear')
@@ -21,28 +25,28 @@ try:
 
         # Record Audio:
         with speech as source:
-            print("Listening...")
+            print("\nListening...")
             audio = r.adjust_for_ambient_noise(source)
             audio = r.listen(source)
 
         # Translate Speech-to-Text:
         recog = r.recognize_google(audio, language = 'en-US')
 
-        # Wake Word 0 (grapefruit)
+        # Fetch
         if recog == 'fetch' :
 
             print("Wake word 'fetch' for fetching detected.")
             # ADD OBJECT DETECTION FUNCTION
                 # Also add fetching function.
         
-        # Wake Word 1 (terminator)
+        # Follow
         if recog == 'follow' :
 
             print("Wake word 'follow' for following detected.")
             # ADD OBJECT DETECTION FUNCTION
                 # Also add following function.
 
-        # Wake Word 2 (view glass)
+        # Selfie
         if recog == 'selfie' :
 
             print("Wake word 'selfie' for selfie detected.")
