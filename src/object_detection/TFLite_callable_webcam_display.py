272ca0ca72ceec4a30c9cbe3fe0cd94cd29e4775
Alice Mwanri <anasirwa@gmail.com>
2022-03-25 19:43:56 -0400

    RE issue #57 implementing basic alignment

--- src/object_detection/TFLite_callable_webcam_display.py
+++ src/object_detection/TFLite_callable_webcam_display.py
@@ -275,8 +275,21 @@ class ObjectDetection:
                         self.side = "left"
                     else:
                         self.side = "right"
-						
-            print(self.objectFound)	
+					
+
+                    cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
+
+                    # Draw label
+                    object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
+                    label = '%s: %f%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
+                    labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
+                    label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
+                    cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
+                    cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
+			
+            cv2.imshow('Object detector', frame)			
+            print(self.objectFound)
+            print(leftArea/rightArea)			
 
             # except KeyboardInterrupt:
             #     print("KeyboardInterrupt at search()")
