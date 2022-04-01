5fcc2a2331abb3f62d20b9ab15a0d4700c43200f
NMK05 Robot <NMK052021@gmail.com>
2022-04-01 15:40:08 -0400

    updated ultrasonic distance and camera servo control

---
+++ src/launcher.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# launcher.sh
+# Navigate to this directory and execute robot.py
+
+cd /home/pi/Capstone/
+python3 src/robot.py 
+
--- src/object_detection/movement/new_camera_test.py
+++ src/object_detection/movement/new_camera_test.py
@@ -1,35 +1,42 @@
+# Imports
 from gpiozero import Servo
 from time import sleep
 import math
 from gpiozero.pins.pigpio import PiGPIOFactory
+
+# Setup
 factory = PiGPIOFactory()
 vertical = Servo(11, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000, pin_factory=factory)
-camera = Servo(23, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000, pin_factory=factory)
-vertical.value = math.sin(math.radians(30))
-camera.max()
+horizontal = Servo(23, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000, pin_factory=factory)
+
 def v_up():
     for i in range (0, 90):
         vertical.value = math.sin(math.radians(i))
         sleep(0.01)
+        
 def v_down():
     for i in range (90,1, -1):
         vertical.value = math.sin(math.radians(i))
         sleep(0.01)
+        
 def c_up():
     for i in range (270, 360):
-        camera.value = math.sin(math.radians(i))
+        horizontal.value = math.sin(math.radians(i))
         sleep(0.01)
+        
 def c_down():
     for i in range (360,270, -1):
-        camera.value = math.sin(math.radians(i))
+        horizontal.value = math.sin(math.radians(i))
         sleep(0.01)
-#vertical.max
-#v_up()
-#sleep(2)
-#v_down()
-#sleep(2)
-#c_up()
-#sleep(2)
-#c_down()
-#vertical.value = math.sin(math.radians(30))
-#camera.max()
+
+# Used to set a vertical positioning of the camera (0 to 360)
+def change_vertical(i):
+    vertical.value = math.sin(math.radians(i))
+
+# Initial Position of the camera (Foward, angled down.)
+def default_pos():
+    vertical.value = math.sin(math.radians(150))
+    horizontal.max()
+
+# Call default position.
+default_pos()
