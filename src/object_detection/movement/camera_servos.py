975d4b3856abe7120d14c9479be4fee5d71174ee
NMK05 Robot <NMK052021@gmail.com>
2022-04-01 15:56:31 -0400

    cleaned up and deleted extra files/code

--- src/object_detection/movement/camera_servos.py
+++ src/object_detection/movement/camera_servos.py
@@ -9,26 +9,6 @@ factory = PiGPIOFactory()
 vertical = Servo(11, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000, pin_factory=factory)
 horizontal = Servo(23, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000, pin_factory=factory)
 
-def v_up():
-    for i in range (0, 90):
-        vertical.value = math.sin(math.radians(i))
-        sleep(0.01)
-        
-def v_down():
-    for i in range (90,1, -1):
-        vertical.value = math.sin(math.radians(i))
-        sleep(0.01)
-        
-def c_up():
-    for i in range (270, 360):
-        horizontal.value = math.sin(math.radians(i))
-        sleep(0.01)
-        
-def c_down():
-    for i in range (360,270, -1):
-        horizontal.value = math.sin(math.radians(i))
-        sleep(0.01)
-
 # Used to set a vertical positioning of the camera (0 to 360)
 def change_vertical(i):
     vertical.value = math.sin(math.radians(i))
